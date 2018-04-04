71b922c8fcf7a90f4733397521bd731217bbb48e
Sergii Lisnyi <Sergii_Lisnyi@epam.com>
2018-04-04 10:02:50 +0300

    add Project

---
+++ Project0/Project0/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Project0
+//
+//  Created by Sergii Lisnyi on 4/3/18.
+//  Copyright © 2018 Sergii Lisnyi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ Project0/Project0/ViewController.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet var digitsButtons: [UIButton]!
+    
+    @IBAction func onButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
+
