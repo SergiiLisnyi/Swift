073cf2a0bde512db288b72afcc6487987648252d
Sergii Lisnyi <Sergii_Lisnyi@epam.com>
2018-04-05 11:16:01 +0300

    fix some bugs with operation +-/*

--- Project0/Project0/ViewController.swift
+++ Project0/Project0/ViewController.swift
@@ -6,21 +6,19 @@ import UIKit
 final class ViewController: UIViewController {
     
     enum Operation: Int {
-        case Divide = 15
-        case Multiple = 14
-        case Subtract = 13
         case Add = 12
+        case Subtract
+        case Multiple
+        case Divide
     }
     
     var numberOnDisplay: Double = 0
     var previosNumber: Double = 0
-    var tmpNumber: Double = 0
-    var perfomingmath = false
+    var operationDuring = false
+    var operationDone = false
     var operation = 0
     
     @IBOutlet weak var displayLabel: UILabel!
-    
- 
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,57 +29,74 @@ final class ViewController: UIViewController {
         previosNumber = 0
         operation = 0
         numberOnDisplay = 0
+        operationDuring = false
+        operationDone = false
     }
     
+    func testDisplay() -> Int {
+        if let displayText = displayLabel.text, let displayInt =  Int(displayText){
+            return displayInt
+        }
+        return 0
+    }
+    
+    
     @IBAction func onButtonDigitsTapped(_ sender: UIButton) {
-        
-        if perfomingmath == true{
-            displayLabel.text = String(sender.tag)
-            numberOnDisplay = Double(displayLabel.text!)!
-            perfomingmath = false
-        } else {
-            guard let displayText = displayLabel.text, let displayInt =  Int(displayText) else { return }
-            let result = displayInt * 10 + sender.tag
-            numberOnDisplay = Double(result)
-            tmpNumber = Double(result)
-            displayLabel.text = String(result)
+        if  operationDone == false {
+            if operationDuring == true {
+                displayLabel.text = String(sender.tag)
+               // numberOnDisplay = Double(displayLabel.text!)!
+                numberOnDisplay = Double(testDisplay())
+                operationDuring = false
+            } else {
+              //  guard let displayText = displayLabel.text, let displayInt =  Int(displayText) else { return }
+                let result = testDisplay() * 10 + sender.tag
+                numberOnDisplay = Double(result)
+                displayLabel.text = String(result)
+            }
+        }
+        else {
+            operationDone = false
+            onButtonDigitsTapped(sender)
         }
     }
     
+    @IBAction func onButtonEqualTapped(_ sender: UIButton) {
+        switch operation {
+            case Operation.Add.rawValue: displayLabel.text = String(previosNumber + numberOnDisplay)
+            case Operation.Subtract.rawValue: displayLabel.text = String(previosNumber - numberOnDisplay)
+            case Operation.Multiple.rawValue: displayLabel.text = String(previosNumber * numberOnDisplay)
+            case Operation.Divide.rawValue: displayLabel.text = String(previosNumber / numberOnDisplay)
+            case Operation.Divide.rawValue: displayLabel.text = String(previosNumber / numberOnDisplay * 100)
+            default: break
+        }
+        operationDone = true
+        operationDuring = false
+        previosNumber = Double(displayLabel.text!)!
+        operation = 0
+        }
     
     @IBAction func onButtonOperationTapped(_ sender: UIButton) {
         
-        guard let displayText = displayLabel.text, let displayInt =  Int(displayText) else { return }
-       
         
-        if displayLabel.text != "" && sender.tag != 11 {
-            previosNumber = Double(displayInt)
-            switch sender.tag {
-            case Operation.Add.rawValue: displayLabel.text = String(previosNumber) + " + "
-            case Operation.Subtract.rawValue: displayLabel.text = String(previosNumber) + " - "
-            case Operation.Multiple.rawValue: displayLabel.text = String(previosNumber) + " * "
-            case Operation.Divide.rawValue: displayLabel.text = String(previosNumber) + " / "
-            case Operation.Divide.rawValue: displayLabel.text = String(previosNumber) + " % "
-            default: return
-            }
-            operation = sender.tag
-            perfomingmath = true
-        } else if sender.tag == 11 {
-            
-            print(previosNumber)
-            print(numberOnDisplay)
-            
-            switch operation {
-            case 12:
-                displayLabel.text = String(previosNumber + numberOnDisplay)
-                tmpNumber = tmpNumber + numberOnDisplay
-            case 13: displayLabel.text = String(previosNumber - numberOnDisplay)
-            case 14: displayLabel.text = String(previosNumber * numberOnDisplay)
-            case 15: displayLabel.text = String(previosNumber / numberOnDisplay)
-            case 16: displayLabel.text = String(previosNumber / numberOnDisplay)
-            default: return
-            }
+        
+        if  operationDuring == false {
+            onButtonEqualTapped(sender)
         }
+        operation = sender.tag
+        operationDuring = true
+        
+        //guard let displayText = displayLabel.text, let displayInt =  Int(displayText) else { return }
+       // previosNumber = Double(displayInt)
+        
+//            switch operation {
+//                case Operation.Add.rawValue: displayLabel.text = String(previosNumber) //+ " + "
+//                case Operation.Subtract.rawValue: displayLabel.text = String(previosNumber) //+ " - "
+//                case Operation.Multiple.rawValue: displayLabel.text = String(previosNumber) //+ " * "
+//                case Operation.Divide.rawValue: displayLabel.text = String(previosNumber) //+ " / "
+//                case Operation.Divide.rawValue: displayLabel.text = String(previosNumber)// + " % "
+//            default: break
+//        }
     }
 }
 
