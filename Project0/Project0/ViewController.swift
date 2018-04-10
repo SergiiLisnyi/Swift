83e263c44196d0901cfb7ca2cf9c8bfd53b69d15
Sergii Lisnyi <Sergii_Lisnyi@epam.com>
2018-04-10 16:13:04 +0300

    after review

--- Project0/Project0/ViewController.swift
+++ Project0/Project0/ViewController.swift
@@ -12,155 +12,128 @@ final class ViewController: UIViewController {
     
     enum Operation: Int {
         case add = 12
-        case subtract = 13
-        case multiple = 14
-        case divide = 15
-        case percent = 16
-        case sin = 30
-        case cos = 31
-        case tan = 32
-        case ctan = 33
-        
+        case subtract
+        case multiple
+        case divide
+        case percent
+        case xy
+        case x2
+        case x3
+        case sin
+        case cos
+        case tan
+        case ctan
+        case ey
+    }
+
+    final let valueE = 2.71828182846
+    
+    var numberOnDisplay: Double {
+        get {
+            if let input = displayLabel.text , let result = Double(input){
+            return result
+            }
+            return 0
+        }
+        set {
+            displayLabel.text = newValue.truncatingRemainder(dividingBy: 1.0) == 0 ? String(format: "%.0f", newValue) : String(newValue)
+        }
     }
     
-//    enum Trigonometric: Int {
-//        case sin = 30
-//        case cos
-//        case tan
-//        case ctan
-//    }
-    
-    
-    
-    var numberOnDisplay = 0.0
     var previosNumber = 0.0
-    var operationDuring = false
-    var operationDone = false
-    var operationTrigonometric = false
-    var operation = 0
+    var isOperationProgress = false
+    var isOperationDone = false
+    var operation: Int!
     
     @IBOutlet weak var displayLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+
     @IBAction func acButton(_ sender: Any) {
-        displayLabel.text = "0"
-        previosNumber = 0
-        operation = 0
         numberOnDisplay = 0
-        operationDuring = false
-        operationDone = false
+        previosNumber = 0
+        operation = nil
+        isOperationProgress = false
+        isOperationDone = false
     }
     
     @IBAction func onButtonDigitsTapped(_ sender: UIButton) {
-        if  operationDone == false {
-            if operationDuring == true {
-                
-                let result = testValueDisplay()
-                if (result.hasPrefix("0.") == true) && (result.count == 2){
-                    displayLabel.text = result + String(sender.tag)
-                    numberOnDisplay = Double(testValueDisplay())!
-                    operationDuring = false
-                    return
-                }
-                displayLabel.text = String(sender.tag)
-                numberOnDisplay = Double(testValueDisplay())!
-                operationDuring = false
-            } else {
-                var result = testValueDisplay() + String(sender.tag)
-                if (result.first == "0") && (result.contains(".") != true) {
-                    result.removeFirst()
-                }
-                numberOnDisplay = Double(result)!
-                displayLabel.text = String(result)
-            }
+        
+        guard let title = sender.currentTitle, let number = Double(title) else{
+            return
         }
-        else {
-            operationDone = false
+        
+        guard !isOperationDone else {
+            isOperationDone = false
             onButtonDigitsTapped(sender)
+            return
+        }
+        
+        if isOperationProgress {
+            numberOnDisplay = number
+            isOperationProgress = false
+        } else {
+            numberOnDisplay = numberOnDisplay * 10 + number
         }
     }
     
-    @IBAction func dot(_ sender: UIButton) {
-        let displayText = testValueDisplay()
-        if operationDuring == true {
-            displayLabel.text = "0."
-        }
-         else if (displayText.contains(".")) {
-            return
-        }
-        else {
-            displayLabel.text = displayText + "."
+    func mathOperation(_ firstOperand: Double, _ secondOperand: Double, _ operation: Operation) -> Double! {
+        switch operation {
+        case .add:
+            return firstOperand + secondOperand
+        case .subtract:
+            return firstOperand - secondOperand
+        case .multiple:
+            return firstOperand * secondOperand
+        case .divide:
+            return firstOperand / secondOperand
+        case .xy:
+            return pow(firstOperand, secondOperand)
+        case .x2:
+            return pow(firstOperand, 2)
+        case .x3:
+            return pow(firstOperand, 3)
+        case .sin:
+            return sin(firstOperand * .pi / 180)
+        case .cos:
+            return cos(firstOperand * .pi / 180)
+        case .tan:
+            return tan(firstOperand * .pi / 180)
+        case .ctan:
+            return pow(tan(firstOperand * .pi / 180), -1)
+        case .ey:
+            return pow(valueE, firstOperand)
+        default:
+            return nil
         }
     }
     
     @IBAction func onButtonEqualTapped(_ sender: UIButton) {
-        switch operation {
-            case Operation.add.rawValue: displayLabel.text = resultString(previosNumber + numberOnDisplay)
-            case Operation.subtract.rawValue: displayLabel.text = resultString(previosNumber - numberOnDisplay)
-            case Operation.multiple.rawValue: displayLabel.text = resultString(previosNumber * numberOnDisplay)
-            case Operation.divide.rawValue: displayLabel.text = resultString(previosNumber / numberOnDisplay)
-            case Operation.percent.rawValue: displayLabel.text = resultString(previosNumber / numberOnDisplay * 100)
-            case Operation.sin.rawValue: displayLabel.text = resultString(sin(numberOnDisplay * .pi / 180))
-            case Operation.cos.rawValue: displayLabel.text = resultString(cos(numberOnDisplay * .pi / 180))
-            case Operation.tan.rawValue: displayLabel.text = resultString(tan(numberOnDisplay * .pi / 180))
-            case Operation.ctan.rawValue: displayLabel.text = resultString((cos(numberOnDisplay * .pi / 180)) /
-                                                                            (sin(numberOnDisplay * .pi / 180)))
-            default: break
+    
+        if operation != nil {
+            let operation = Operation(rawValue: self.operation)!
+            numberOnDisplay = mathOperation(previosNumber, numberOnDisplay, operation)
         }
-        operationDone = true
-        operationDuring = false
-        previosNumber = Double(displayLabel.text!)!
-        operation = 0
-        }
+        isOperationDone = true
+        isOperationProgress = false
+        previosNumber = numberOnDisplay
+        operation = nil
+    }
     
     @IBAction func onButtonOperationTapped(_ sender: UIButton) {
-        if  operationDuring == false {
+        if  !isOperationProgress {
             onButtonEqualTapped(sender)
         }
         operation = sender.tag
-        operationDuring = true
+        isOperationProgress = true
     }
     
-    
-    @IBAction func onButtonTrigonometricTaped(_ sender: UIButton) {
-        operationTrigonometric = true
-        
-            switch sender.tag {
-                case Operation.sin.rawValue: displayLabel.text = "sin("
-    
-                default: break
+    @IBAction func onButtonUnaryTapped(_ sender: UIButton) {
+        guard let operation = Operation(rawValue: sender.tag) else {
+            return
         }
-        operation = sender.tag
-        operationDuring = true
+        numberOnDisplay = mathOperation(numberOnDisplay, previosNumber, operation)
     }
     
-    
-    @IBAction func onButtonExponentiationTapped(_ sender: UIButton) {
-        //if  operationDuring == false {
-            if sender.tag == 20 {
-                displayLabel.text = resultString(numberOnDisplay * numberOnDisplay)
-                //operationDone = true
-            } else if sender.tag == 21 {
-            displayLabel.text = resultString(numberOnDisplay * numberOnDisplay * numberOnDisplay)
-            //operationDone = true
-        }
-       // }
-    }
-    
-    
-    
-    func resultString(_ value : Double) -> String {
-        return value.truncatingRemainder(dividingBy: 1.0) == 0 ? String(format: "%.0f", value) : String(value)
-    }
-    
-    func testValueDisplay() -> String {
-        if let displayText = displayLabel.text {
-            return displayText
-        }
-        return ""
-    }
 }
+    
+
 
